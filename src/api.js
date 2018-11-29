9416b25558aa7a7516de2ad941349ae9869f33c2
Haikel Fazzani <haikel.fazzani@gmail.com>
2018-11-29 01:29:22 +0100

    update app

--- src/api.js
+++ src/api.js
@@ -5,20 +5,20 @@ import EventEmitter from "./event-emiter";
 
 export class AudioPlayer {
 
-    constructor(parentElment, childs = 'li', activeClass = "active", loop = false, loopAll = false) {
+    constructor(parentId, childs = 'li', activeClass = "active", loop = false, loopAll = false) {
         this.emitter = new EventEmitter();
         this.audio = new Audio();
         this.tracks = []; // list of tracks
         this.currentTrackIndex = 0;
         this.i = 0; // index of each audio stored in the array : tracks
-        this.parentElment = parentElment; // example ul elements
+        this.parentId = parentId; // example ul elements
         this.childs = childs; // example li elements
         this.duration = this.audio.duration;
         this.hasChild = false; // parent element (ul) has childs (li)
         this.activeClass = activeClass; // active track class (css)
-        this.loopAll = loopAll; // loop all tracks
-        this.audio.loop = loop; // loop single track
-        this.loopAllTacks();
+        this.loopAll = loopAll; // loop all tracks ( : boolean)
+        this.audio.loop = loop; // loop single track ( : boolean)
+        this.loopAllTacks(); // ( : method)
     }
 
     addAudio(event) {
@@ -33,10 +33,10 @@ export class AudioPlayer {
     }
 
     // on click on audio in the list , the audio play the current
-    playCurrentAudio() {
+    playCurrentTack() {
         this.emitter.on('haschild', (hasChild) => {
             if (hasChild) {
-                [...this.parentElment.childNodes].map((child, index) => {
+                [...this.parentId.childNodes].map((child, index) => {
                     this.emitter.removeListener('haschild', this.hasChild);
                     child.onclick = () => {
                         this.removeActiveClass();
@@ -52,7 +52,7 @@ export class AudioPlayer {
 
     // create li list from tracks after converting links to urls object
     createPlayList(url) {
-        this.createElemnt(this.parentElment, this.childs, url.name);
+        this.createElemnt(this.parentId, this.childs, url.name);
     }
 
     /* ------- Audio Controls -------- */
@@ -112,6 +112,7 @@ export class AudioPlayer {
         this.audio.loop = false;
         this.loopAll = this.loopAll ? false : true;
         this.emitter.emit('playall', this.loopAll);
+        return this.loopAll;
     }
 
     loopAllTacks() {
@@ -135,16 +136,16 @@ export class AudioPlayer {
     /* __________ Methods to handle the tracks list Elements */
 
     addActiveClassByIndex(index = 0) {
-        this.parentElment.childNodes[index].classList.add(this.activeClass);
+        this.parentId.childNodes[index].classList.add(this.activeClass);
     }
 
     // remove class 'active' from the previous active track
     removeActiveClass() {
-        [...this.parentElment.childNodes].map(child => child.classList.remove(this.activeClass));
+        [...this.parentId.childNodes].map(child => child.classList.remove(this.activeClass));
     }
 
     removeActiveClassByIndex(index = 0) {
-        this.parentElment.childNodes[index].classList.remove(this.activeClass);
+        this.parentId.childNodes[index].classList.remove(this.activeClass);
     }
 
 
